db2213fe251ba9a406f0d0bc500f463047d8cd67
Sumaiya02 <35718906+Sumaiya02@users.noreply.github.com>
2018-03-11 00:42:54 +0600

    DonnerViewModel added

---
+++ V1/BdAidSln/BdAid/Models/DonnerViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BdAid.Models
+{
+    public class DonnerViewModel
+    {
+        public string post_id { get; set; }
+        [Required]
+        [Display]
+        public string place_name { get; set; }
+        [Required]
+        [StringLength(11)]
+        public string contact_num { get; set; }
+
+        //public string payType { get; set; }
+
+
+
+
+    }
+}
