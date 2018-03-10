58001bd1e804d9e86576916511ab3f500ee98688
Sumaiya02 <35718906+Sumaiya02@users.noreply.github.com>
2018-03-11 00:36:10 +0600

    Donner Controller

---
+++ V1/BdAidSln/BdAid/Controllers/DonnerController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BdAid.Controllers
+{
+    public class DonnerController : Controller
+    {
+        // GET: Donner
+        public ActionResult ViewPage()
+        {
+            return View();
+        }
+
+        public ActionResult ViewRespond()
+        {
+            return View();
+        }
+
+
+    }
+}
